26c0ae903e0e536107104b6f1634f2fb93151950
AllaKulchitskaya <alla070@list.ru>
2022-12-14 20:50:47 +0300

    Домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        boolean success = Data.checkData("christina305", "DogAndCat105", "DogAndCat105");
+        if (success) {
+            System.out.println("Данные введены верно");
+        } else {
+            System.out.println("Данные введены неверно");
+        }
     }
 }
